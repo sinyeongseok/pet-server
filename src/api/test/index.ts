4acc47941ceb2b770162b09a0c33b9a3078d6aa8
sinyeongseok <sinyeongseok99@gmail.com>
2023-04-09 18:28:57 +0900

    feat: koyeb env 테스트

--- src/api/test/index.ts
+++ src/api/test/index.ts
@@ -3,7 +3,9 @@ import express, { Request, Response, NextFunction } from "express";
 const app: express.Application = express();
 
 app.get("/", (req: Request, res: Response) => {
-  res.status(200).json({ message: "pet server test!!!" });
+  res
+    .status(200)
+    .json({ message: "pet server test!!!", testkey: process.env.test_key });
 });
 
 export default app;
